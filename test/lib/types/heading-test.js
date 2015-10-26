748abd05c3d3fe4e2dac88fe023c7b20ca1c806f
Jonathan Clem <jotclem@gmail.com>
2015-10-26 19:36:59 -0400

    Add Heading

---
+++ test/lib/types/heading-test.js
@@ -0,0 +1,18 @@
+import Heading    from '../../../lib/types/heading';
+import { expect } from 'chai';
+import { wrap   } from '../../../lib/brackets';
+
+describe('Heading', () => {
+  describe('.match', () => {
+    it('matches a normal heading line', () => {
+      const source = `# Heading`;
+      expect(Heading.match(source)).to.be.an.instanceof(Heading);
+    });
+
+    it('determines its level', () => {
+      const source = `## Heading`;
+      expect(Heading.match(source).level).to.eql(2);
+    });
+  });
+});
+
