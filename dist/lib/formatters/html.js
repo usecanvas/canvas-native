1c2ed25f254492b086d088d250de31b29f2aea36
Jonathan Clem <jotclem@gmail.com>
2015-10-29 16:34:53 -0400

    Use markdown-it for markdown to HTML

--- dist/lib/formatters/html.js
+++ dist/lib/formatters/html.js
@@ -7,16 +7,24 @@ exports['default'] = format;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-var _marked = require('marked');
+var _markdownIt = require('markdown-it');
 
-var _marked2 = _interopRequireDefault(_marked);
+var _markdownIt2 = _interopRequireDefault(_markdownIt);
+
+var _markdownItCheckbox = require('markdown-it-checkbox');
+
+var _markdownItCheckbox2 = _interopRequireDefault(_markdownItCheckbox);
 
 var _markdown = require('./markdown');
 
 var _markdown2 = _interopRequireDefault(_markdown);
 
+var renderer = new _markdownIt2['default']({
+  linkify: true
+}).use(_markdownItCheckbox2['default']);
+
 function format(native) {
-  return (0, _marked2['default'])((0, _markdown2['default'])(native));
+  return renderer.render((0, _markdown2['default'])(native));
 }
 
 module.exports = exports['default'];
