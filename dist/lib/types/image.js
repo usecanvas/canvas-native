07a3e177f341ffa05092c9199204066f04314b84
Jonathan Clem <jotclem@gmail.com>
2015-12-07 16:45:34 -0800

    Remove alt/title from to markdown

--- dist/lib/types/image.js
+++ dist/lib/types/image.js
@@ -4,8 +4,6 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
-
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
@@ -58,15 +56,8 @@ var Image = (function (_Type) {
   }, {
     key: 'toMarkdown',
     value: function toMarkdown(prev, next) {
-      var _content$split = this.content.split(' ');
-
-      var _content$split2 = _slicedToArray(_content$split, 2);
-
-      var url = _content$split2[0];
-      var alt = _content$split2[1];
-
-      url = url.replace(/ /g, '%20');
-      var markdown = '![](' + url + (alt ? ' ' + alt : '') + ')';
+      var url = this.meta.url.replace(/ /g, '%20');
+      var markdown = '![](' + url + ')';
 
       if (!next) {
         return markdown;
